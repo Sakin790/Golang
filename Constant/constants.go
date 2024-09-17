7061c18fc66119c081ad4bdff632cade47165d8f
sakin_dev@79 <sakinislam79@gmail.com>
2024-09-17 16:31:31 +0600

    added

---
+++ Constant/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name string = "Sakin"
+	fmt.Println(name)
+
+
+     const (
+		port=500
+		host= "LocalHost"
+	)
+	fmt.Println(host)
+}
---
+++ DataType/Variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "GOLANG"
+	fmt.Println(name)
+	userName:="Mahid"
+	fmt.Println(userName)
+
+	
+	const score int = 100
+	fmt.Println(score)
+}
